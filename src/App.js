02ed62e8891c5ff122fa6a87ccf89e8dd5bd66e7
github username <github email address>
2019-02-26 20:49:01 -0500

    show login form on browser

--- src/App.js
+++ src/App.js
@@ -6,7 +6,14 @@ class App extends Component {
   render() {
     return <div className="App">
         <Header />
+          <h1>Log In</h1>
+            <form>
+            <input name="username" placeholder="Username"/><br/>
+            <input type='password' name="password" placeholder="Password"/><br/>
+            <button type="submit">Login</button>
+          </form>
       </div>;
+
   }
 }
 
