380cc45e21b1abc9715916b572cdb5b488babd84
github username <github email address>
2019-03-06 07:10:22 -0500

    trying to get rails c to run so I can try to create a post and trying to get postman to create a post

--- finstagram-api/app/controllers/api/posts_controller.rb
+++ finstagram-api/app/controllers/api/posts_controller.rb
@@ -1,14 +1,48 @@
 class Api::PostsController < ApplicationController
 
+  before_action :set_post, only: [:show, :update, :destroy]
+
   def index
     render json: Post.all
   end
 
   def create
     post = Post.new(post_params)
+    if post.save
+      render json: post
+    else
+      render json: { message: post.errors }, status: 400
+    end
   end
 
   def show
     render json: Post.find_by(id: params[:id])
   end
+
+  def update
+    if @post.update(post_params)
+      render json: @post
+    else
+      render json: { message: post.errors }, status: 400
+    end
+  end
+
+  def desroy
+    if @post.destroy
+      render status: 204
+    else
+      render json: { message: "Unable to remove this post"}, status: 400
+    end
+  end
+
+  private
+
+    def set_post
+      @post = Post.find_by(id: params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:name, :img_url, :comment)
+    end
+
 end
