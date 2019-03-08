13fd54de3b678149c3b6d8fa135fc6ef6f3dbe9f
github username <github email address>
2019-03-08 06:36:36 -0500

    start adding redux and redux-thunk to client

--- finstagram-client/src/containers/Posts.js
+++ finstagram-client/src/containers/Posts.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PostCard from '../components/PostCard';
+import PostForm  from './PostForm';
 import './Post.css';
 
-const Posts = (props) => (
-  <div className="PostsContainer">
-    <h1>Posts</h1>
-    {props.posts.map(post => <PostCard key={post.id} post={post} />)}
-  </div>
-);
+class Posts extends Component {
+
+  render() {
+    return (
+      <div className="PostsContainer">
+        <h1>Posts</h1>
+        {this.props.posts.map(post => <PostCard key={post.id} post={post} />)}
+        <PostForm />
+      </div>
+    )
+  }
+}
 
 export default Posts
