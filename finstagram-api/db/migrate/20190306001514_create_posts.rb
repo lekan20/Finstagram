4abd49d1c6cb6e7831719f589481761ff1e179b5
github username <github email address>
2019-03-16 08:03:24 -0400

    update posts table to create belongs to relationship with user

--- finstagram-api/db/migrate/20190306001514_create_posts.rb
+++ finstagram-api/db/migrate/20190306001514_create_posts.rb
@@ -1,6 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
+      t.belongs_to :user
       t.string :name
       t.string :img_url
       t.string :comment
