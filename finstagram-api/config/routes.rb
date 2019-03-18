7178f597f1385ce334da62aac66d4c85d637a141
github username <github email address>
2019-03-18 07:25:33 -0400

    add user routes

--- finstagram-api/config/routes.rb
+++ finstagram-api/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
+    
+    get 'user', to: 'users#show', as: 'user_show'
+    post 'signup', to: 'users#create', as: 'user_signup'
+    post 'login', to: 'users#login', as: 'user_login'
+
     resources :posts, except: [:new, :edit]
   end
 end
