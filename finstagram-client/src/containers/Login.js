a3a5cec9f4911071ab7aa60058171af992460f91
github username <github email address>
2019-03-19 20:54:37 -0400

    update login component and add userActions

--- finstagram-client/src/containers/Login.js
+++ finstagram-client/src/containers/Login.js
@@ -1,19 +1,47 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 import { withRouter, Link } from 'react-router-dom'
 
+import { loginUser } from '../actions/userActions'
+
 class Login extends Component {
-  state = {
+  constructor() {
+    super()
+
+    this.state = {
       username: 'demo',
       password: 'password'
     }
 
+    this.onSubmit = this.onSubmit.bind(this)
+    this.onChange = this.onChange.bind(this)
+  }
+
+  onChange(e) {
+    const field = e.target.name
+    let state = this.state
+
+    state[field] = e.target.value
+    this.setState(state)
+  }
+
+  onSubmit(e) {
+    e.preventDefault()
+
+    const user = this.state
+    this.props.loginUser(user, () => this.props.history.push('/'))
+  }
+
   render() {
+    const { username, password } = this.state
+
     return (
       <>
         <h1>Log In</h1>
         <form onSubmit={ this.onSubmit }>
-          <input name="username" placeholder="Username"/><br/>
-          <input type='password' name="password" placeholder="Password" /><br/>
+          <input name="username" placeholder="Username" value={ username } onChange={ this.onChange }/><br/>
+          <input type='password' name="password" placeholder="Password" value={ password } onChange={ this.onChange }/><br/>
           <button type="submit">Login</button>
         </form>
         <Link to='/signup'>Sign Up</Link>
@@ -22,5 +50,8 @@ class Login extends Component {
   }
 }
 
+const mapDispatchToProps = dispatch => bindActionCreators({
+  loginUser
+}, dispatch)
 
-export default Login
+export default withRouter(connect(null, mapDispatchToProps)(Login))
