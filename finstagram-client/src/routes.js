47ae6ca3fe6d90819bd1714bcf806ad73da3a2f9
github username <github email address>
2019-03-21 06:15:23 -0400

    Test adding TodoList route and container

--- finstagram-client/src/routes.js
+++ finstagram-client/src/routes.js
@@ -3,10 +3,12 @@ import { BrowserRouter, Switch, Route, Redirect, Link } from 'react-router-dom'
 
 import Login from './containers/Login'
 import Signup from './containers/Signup'
+import TodoList from './containers/TodoList'
 
 export default (
   <BrowserRouter>
     <Switch id='routes'>
+      <Route exact path='/' render={ () =>  loggedIn() ? <TodoList/> : <Redirect to="/login"/> }/>
       <Route exact path='/' render={ () =>  <Redirect to="/login"/> }/>
       <Route path='/signup' component={ () => loggedIn() ? <Redirect to="/"/> : <Signup/> }/>
       <Route path='/login' component={ () => loggedIn() ? <Redirect to="/"/> : <Login/> }/>
