8b152709e569cf306c53c2e6796572245b044b1d
github username <github email address>
2019-03-24 20:26:00 -0400

    update serializer to try to grab username and firstname from the post's user. haven't figured it out yet

--- finstagram-client/src/components/Post.js
+++ finstagram-client/src/components/Post.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import  "./Post.css";
 
-const Post = ({ post: { id, user_id, img_url, caption }}) =>
-  <div>
-    <img src={img_url} />
-    <p>{ caption }</p>
-  </div>
+const Post = ({ post: { id, img_url, caption }}) =>
+  <article className="Post">
+    <header>
+      <div className="Post-user">
+        <div className="Post-user-nickname">
+          <span> </span>
+        </div>
+      </div>
+    </header>
+    <div className="Post-image">
+      <div className="Post-image-bg">
+        <img src={img_url} />
+      </div>
+    </div>
+    <div className="Post-caption">
+      <span> </span> { caption }
+    </div>
+  </article>
 
 export default Post
